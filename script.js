7ccad025b2a7838e3a0ea0a3f02398d28e3858b9
Endri-Tosku <endritosku001@gmail.com>
2025-12-04 16:44:38 +0100

    feat: svolto l'es. dei dadi

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* Mail
+/* 
+Mail
 Crea una lista di email di invitati ad una festa.
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
@@ -10,23 +11,16 @@ Non è necessario provvedere alla validazione delle email
 
 Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 
-Consigli del giorno:
-scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-si ma noi cosa vogliamo fare?
-torniamo a scrivere in italiano
-proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" */
+
+
 
 
 // LISTA EMAIL DI INVITATI ALLA FESTA
 
-const invitati = [
+/* const invitati = [
     "anna@gmail.com",
     "luca@gmail.com",
     "mario@gmail.com",
@@ -48,4 +42,23 @@ for (let i = 0; i < invitati.length; i++) {
     } else {
         console.log("Spiacente, non hai trovato il biglietto");
     }
+} */
+
+
+
+
+// GIOCO DEI DATI
+
+const userNumber = Number(prompt("Inserisci un numero da 1 a 6"));
+const computerNumber = Math.floor(Math.random() * 6) + 1;
+
+let punteggioUser = userNumber
+let punteggioComputer = computerNumber
+
+if (punteggioUser > punteggioComputer) {
+    console.log("hai vinto");
+} else if (punteggioUser < punteggioComputer) {
+    console.log("ha vinto il computer");
+} else {
+    console.log("hai pareggiato");
 }
