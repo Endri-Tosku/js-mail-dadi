6317b8ed1c5b96f87bd416f51457b01e16f4f865
Endri-Tosku <endritosku001@gmail.com>
2025-12-04 14:57:17 +0100

    feat: inserita la lista della email

---
+++ bonus/scripr.js
@@ -0,0 +1,7 @@
+/* Crea un array vuoto. 
+ Chiedi per 6 volte all’utente di inserire un numero, 
+ se è dispari inseriscilo nell’array */
+
+
+/* L’utente inserisce due parole in successione, con due prompt. 
+ Il software stampa prima la parola più corta, poi la parola più lunga. */
